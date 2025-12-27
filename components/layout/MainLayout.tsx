a32eebb4923bf7553f6ce8acdd3c1e76e75a5f5f
JahongLiu <jahongliu@gmail.com>
2025-12-27 11:10:54 -0500

    Make the AIchat fixed for better UX

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -17,16 +17,22 @@ export default function MainLayout({ children }: MainLayoutProps) {
         height: "calc(100vh - 4rem)",
         display: "flex",
         flexDirection: "column",
+        overflow: "hidden",
       }}
     >
       <div
         className={`grid gap-8 flex-1 items-stretch ${
           isFocusMode ? "grid-cols-1 lg:grid-cols-2" : "grid-cols-1"
         }`}
-        style={{ gridTemplateRows: "1fr" }}
+        style={{
+          gridTemplateRows: "1fr",
+          minHeight: 0,
+          maxHeight: "100%",
+          overflow: "hidden",
+        }}
       >
         <div className="min-w-0 overflow-y-auto">{children}</div>
-        <div className="h-full min-h-0 flex">
+        <div className="h-full min-h-0 flex overflow-hidden">
           <AIChat />
         </div>
       </div>
