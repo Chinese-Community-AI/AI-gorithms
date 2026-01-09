6ac77b428b9443ecf08e0454469ddbce9fc77ef3
JahongLiu <jahongliu@gmail.com>
2026-01-09 11:00:32 -0500

    Revise content

--- content/articles/basics/10-sorting-algorithms/intro.md
+++
@@ -1,6 +0,0 @@
-This chapter provides a comprehensive explanatio of 10 classic sorting algorithms, including core principles, visualization and code implementation.
-
-For beginners, it is recommended to complete the foundational knowledge of data structures before studying this chapter.
-
-For quick mastery readers, you may skip the code implementation section of this chapter, but you should understand the core principles of each sorting algorithm.
-
--- lib/navigation.ts
+++ lib/navigation.ts
@@ -50,6 +50,14 @@ export const navigation: NavItem[] = [
             href: "/basics/03-hash-tables/hashmap-basics",
           },
           {
+            title: "Linked Hash Map",
+            href: "/basics/03-hash-tables/linked-hash-map",
+          },
+          {
+            title: "Array Hash Map",
+            href: "/basics/03-hash-tables/array-hash-map",
+          },
+          {
             title: "Hash Table Variations",
             href: "/basics/hash-table-variations",
             children: [
@@ -88,114 +96,6 @@ export const navigation: NavItem[] = [
                 href: "/basics/implement-hashmap/two-implementations-of-linear-probing",
               },
             ],
-          },
-        ],
-      },
-      {
-        title: "10 Sorting Algorithms",
-        href: "/basics/10-sorting-algorithms",
-        children: [
-          {
-            title: "Explore Selection Sort",
-            href: "/basics/10-sorting-algorithms/explore-selection-sort",
-          },
-          {
-            title: "Intro",
-            href: "/basics/10-sorting-algorithms/intro",
-          },
-          {
-            title: "Metrics Of Sorting Algorithms",
-            href: "/basics/10-sorting-algorithms/metrics-of-sorting-algorithms",
-          },
-        ],
-      },
-      {
-        title: "Binary Tree Structure And Traversal",
-        href: "/basics/binary-tree-structure-and-traversal",
-        children: [
-          {
-            title: "Binary Tree Basic And Common Types",
-            href: "/basics/binary-tree-structure-and-traversal/binary-tree-basic-and-common-types",
-          },
-          {
-            title: "Binary Tree Recursive Level Traversal",
-            href: "/basics/binary-tree-structure-and-traversal/binary-tree-recursive-level-traversal",
-          },
-          {
-            title: "N Ary Tree Recursive Level Traversal",
-            href: "/basics/binary-tree-structure-and-traversal/n-ary-tree-recursive-level-traversal",
-          },
-          {
-            title: "Use Cases Of Dfs Bfs",
-            href: "/basics/binary-tree-structure-and-traversal/use-cases-of-dfs-bfs",
-          },
-        ],
-      },
-      {
-        title: "Binary Tree Variations",
-        href: "/basics/binary-tree-variations",
-        children: [
-          {
-            title: "Bianry Heap Priority Queue Implementation",
-            href: "/basics/binary-tree-variations/bianry-heap-priority-queue-implementation",
-          },
-          {
-            title: "Binary Heap Basic",
-            href: "/basics/binary-tree-variations/binary-heap-basic",
-          },
-          {
-            title: "Data Compression And Huffman Tree",
-            href: "/basics/binary-tree-variations/data-compression-and-huffman-tree",
-          },
-          {
-            title: "Red Black Trees Basics",
-            href: "/basics/binary-tree-variations/red-black-trees-basics",
-          },
-          {
-            title: "Segment Tree Basics",
-            href: "/basics/binary-tree-variations/segment-tree-basics",
-          },
-          {
-            title: "Treemap Structure Visualization",
-            href: "/basics/binary-tree-variations/treemap-structure-visualization",
-          },
-          {
-            title: "Trie Digital Tree Prefix Tree Basics",
-            href: "/basics/binary-tree-variations/trie-digital-tree-prefix-tree-basics",
-          },
-        ],
-      },
-      {
-        title: "Graph Structure Algorithm Overview",
-        href: "/basics/graph-structure-algorithm-overview.md",
-        children: [
-          {
-            title: "Basic Terminology In Graph Theory",
-            href: "/basics/graph-structure-algorithm-overview.md/basic-terminology-in-graph-theory",
-          },
-          {
-            title: "Eulerian Graph And One Stroke Game",
-            href: "/basics/graph-structure-algorithm-overview.md/eulerian-graph-and-one-stroke-game",
-          },
-          {
-            title: "Graph Shortest Path Algo Overview",
-            href: "/basics/graph-structure-algorithm-overview.md/graph-shortest-path-algo-overview",
-          },
-          {
-            title: "Graph Structure Code Implementation",
-            href: "/basics/graph-structure-algorithm-overview.md/graph-structure-code-implementation",
-          },
-          {
-            title: "Graph Sturecture Dfs Bfs Traversal",
-            href: "/basics/graph-structure-algorithm-overview.md/graph-sturecture-DFS-BFS-traversal",
-          },
-          {
-            title: "Minimm Spanning Tree Algo Overview",
-            href: "/basics/graph-structure-algorithm-overview.md/minimm-spanning-tree-algo-overview",
-          },
-          {
-            title: "Union Find Basics",
-            href: "/basics/graph-structure-algorithm-overview.md/union-find-basics",
           },
         ],
       },
